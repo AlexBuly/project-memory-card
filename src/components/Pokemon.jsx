ef7d66cde744fe89f6cc219cd294bf4af493947f
Alex Buly <asbuly@icloud.com>
2025-08-13 20:42:57 -0400

    Update score when clicked and reset if visited is clicked

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -3,6 +3,8 @@ import { Card } from "./Card";
 
 export function Pokemon() {
     const [visited, setVisited] = useState([]);
+    const [currentScore, setCurrentScore] = useState(0);
+    const [bestScore, setBestScore] = useState(0);
 
     const pokemonNames = [
         "pikachu",
@@ -23,14 +25,19 @@ export function Pokemon() {
     ];
 
     const addVisited = (name) => {
-        setVisited(prev => prev.includes(name) ? prev : [...prev, name])
-        console.log(visited);
+        if (visited.includes(name)) {
+            setBestScore(prevBest => Math.max(prevBest, currentScore));
+            setCurrentScore(0);
+            setVisited([]);
+        } else {
+            setCurrentScore(prevScore => prevScore + 1);
+            setVisited(prev => [...prev, name]);
+        }
     };
-    /////// COMMIT ///////////////
     return (
         <>
-            <p>Best Score: 0 </p>
-            <p>Current Score: {visited.length}</p>
+            <p>Best Score: {bestScore} </p>
+            <p>Current Score: {currentScore}</p>
             <h2>All Pokemons:</h2>
             <div style={{ display: "grid", gridTemplateRows: "1fr 1fr", gridTemplateColumns: "repeat(5, 1fr)", gap: "1rem", flexWrap: "wrap" }}>
                 {pokemonNames.map(name => (
