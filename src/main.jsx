d5ceb14e17b7142b2e0132d5a8c5ec5d22c1f726
Alex Buly <asbuly@icloud.com>
2025-08-21 13:36:31 -0400

    Update styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/App.jsx'
-import { Score } from './components/Score.jsx'
-import { Card } from './components/Card.jsx'
 import './styles/App.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Score />
-    <div className="cards">
-        <Card />
-    </div>
   </StrictMode>,
 )
